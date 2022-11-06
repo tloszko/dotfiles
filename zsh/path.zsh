b26f0599e05203f1d94b3b8e4c3a09c1b4909ca6
Tomasz Łoszko <tloszko@liveperson.com>
2022-11-06 20:43:20 +0100

    move from zplugin to zinit

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
+# export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/postgresql@11/lib"
+# export CPPFLAGS="-I/usr/local/opt/postgresql@11/include"
 
-export LDFLAGS="-L/usr/local/opt/postgresql@11/lib"
-export CPPFLAGS="-I/usr/local/opt/postgresql@11/include"
 export PATH=/bin:$PATH
 export PATH=/sbin:$PATH
 export PATH=/usr/bin:$PATH
@@ -11,5 +11,6 @@ export PATH=/usr/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+# export PATH=$(brew --prefix ansible@2.9)/bin:$PATH
 
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
